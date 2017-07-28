a3f8546e76d33e52b332d28d257c758ae2a5a0a5
Johan Segerfeldt <johan.segerfeldt@me.com>
2017-07-28 07:08:29 +0200

    Refactor: Only create Game in one place

--- tests/bowling/ScoringTests.java
+++ tests/bowling/ScoringTests.java
@@ -5,25 +5,24 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ScoringTests {
+
+    private final Game game = new Game();
+
     @Test
     void noRolls_noScore() {
-        Game game = new Game();
         assertEquals(0, game.score());
     }
 
     @Test
     void oneRoll_scoreIsNumberOfPins() {
-        Game game = new Game();
         game.roll(1);
         assertEquals(1, game.score());
     }
 
     @Test
     void teoRolls_scoreIsTotalNumberOfPins() {
-        Game game = new Game();
         game.roll(1);
         game.roll(1);
         assertEquals(2, game.score());
     }
-
 }
